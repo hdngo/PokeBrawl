cb933cc68fa15542ba59ee586af01c458bd0b7ef
Harvey Ngo <harveydngo@gmail.com>
2015-07-02 10:34:24 -0700

    add mute music control

--- js/battle-music.js
+++ js/battle-music.js
@@ -1,4 +1,5 @@
 var battleBGM = new Audio('./audio/rival-battle.mp3')
 battleBGM.volume =0.025;
 battleBGM.loop = true;
-battleBGM.play()
+battleBGM.play();
+
--- js/battle.js
+++ js/battle.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
 	$('.move1').click(function(){
 		$('.move1').html(sessionStorage.getItem("pokemon"));
 	});
+	$('.unmuted').click(function(){
+		if($(this).hasClass('unmuted')){
+			$(this).removeClass('unmuted');
+		$(this).addClass('muted');
+		battleBGM.volume = 0;
+	}
+	else if($(this).hasClass('muted')){
+		$(this).removeClass('muted');
+		$(this).addClass('unmuted');
+		battleBGM.volume = 0.025;
+	}
+	});
 });
